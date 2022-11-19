e7d128eb9b1bf12765512605b7e795194c78b2e4
BiRuVN <ngngquang99@gmail.com>
2022-11-20 00:10:22 +0700

    add aff link

--- script.js
+++ script.js
@@ -36,45 +36,52 @@ function call_api(is_onload=true) {
         console.log(objectData['data']);
         let tableData = "";
         let img_url = "";
-        let btn = "";
+        let btn = `<a href="#" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
         objectData['data'].map((values) => {
-            if (values.discount_code == ""){
-                btn = `<a href="#" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`
-            } else {
-                btn = `<div id="copy-code" class="btn btn-primary justify-content-center" style="display:block;font-size:large;" onclick="mycopy('${values.discount_code}')">
-                            <i class="far fa-copy me-2"></i>
-                            COPY MÃ
-                        </div>`
-            }
             switch (values.type){
                 case "Tiki":
                     img_url = "./img/ma-giam-gia-tiki.webp";
+                    btn = `<a href="https://tinyurl.com/2zgghzsl" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Shopee":
                     img_url = "./img/shopee-logo-40482.png";
+                    btn = `<a href="https://tinyurl.com/2qdfvzla" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Lazada":
                     img_url = "./img/ma-giam-gia-lazada.webp";
+                    btn = `<a href="https://shorten.asia/XTUESTQU" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Grab":
                     img_url = "./img/grab-logo.webp";
+                    btn = `<a href="https://www.grab.com/vn/" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Now":
                     img_url = "./img/logo-shopeefood.webp";
+                    btn = `<a href="https://tinyurl.com/2kkpxove" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Nguyen-Kim":
                     img_url = "./img/ma-giam-gia-nguyen-kim.webp";
+                    btn = `<a href="https://shorten.asia/efjkt4cu" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Sendo":
                     img_url = "./img/ma-giam-gia-sendo.webp";
+                    btn = `<a href="https://www.sendo.vn/" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 case "Fahasha":
                     img_url = "./img/logo-fahasa.webp";
+                    btn = `<a href="https://shorten.asia/K1dRCn9C" class="btn btn-primary justify-content-center mt-2" style="display:block;font-size:large;">NHẬN ƯU ĐÃI</a>`;
                     break;
                 default:
                     img_url = "./img/black_default.png";
                     break;
             }
+            if (values.discount_code != "") {
+                btn = `<div id="copy-code" class="btn btn-primary justify-content-center" style="display:block;font-size:large;" onclick="mycopy('${values.discount_code}')">
+                            <i class="far fa-copy me-2"></i>
+                            COPY MÃ
+                        </div>`
+            }
+            
             tableData += 
             `<div class ="col-12 col-sm-6 col-md-4 p-2 align-items-stretch" style="width: 33%;">
                 <div class="card">
